a8a8ae60e715df7b53f3622d27cf11ea1e219e3b
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-07-01 19:44:59 -0400

    Game window styling

--- src/game.js
+++ src/game.js
@@ -1,9 +1,9 @@
 kaboom({
     global: true,
-    fullscreen: true,
     scale: 1,
     debug: true,
     clearColor: [0, 0, 0, 1],
+    canvas: document.getElementById("game"),
 });
 
 const MOVE_SPEED = 120;
