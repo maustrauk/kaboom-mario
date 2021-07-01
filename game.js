745e706c641ae5eb976ec453ca672dcd8fe363fb
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-07-01 00:45:29 -0400

    Adding player to scene

--- game.js
+++ game.js
@@ -53,6 +53,13 @@ scene("game", () => {
     }
 
     const gameLevel = addLevel(map, levelCfg)
+
+    const player = add([
+        sprite('mario'), solid(),
+        pos(30, 0),
+        body(),
+        origin('bot')
+    ])
 })
 
 start("game")
