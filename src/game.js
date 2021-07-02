432a26dd1252e6efd667bb819c89613008d229c4
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-07-01 20:15:06 -0400

    Seperate sprites.js

--- src/game.js
+++ src/game.js
@@ -18,20 +18,6 @@ let CURRENT_MOVE_SPEED = MOVE_SPEED;
 let isJumping = true;
 
 
-loadRoot('src/assets/');
-loadSprite('coin', 'coin.png');
-loadSprite('evil-shroom', 'evil-shroom.png');
-loadSprite('brick', 'brick.png');
-loadSprite('block', 'block.png');
-loadSprite('mario', 'mario.png');
-loadSprite('mushroom', 'mushroom.png');
-loadSprite('surprise', 'surprise.png');
-loadSprite('unboxed', 'unboxed.png');
-loadSprite('pipe-top-left', 'pipe-top-left.png');
-loadSprite('pipe-top-right', 'pipe-top-right.png');
-loadSprite('pipe-bottom-left', 'pipe-bottom-left.png');
-loadSprite('pipe-bottom-right', 'pipe-bottom-right.png');
-
 scene("game", ({ score }) => {
     layers(['bg', 'obj', 'ui'], 'obj');
 
---
+++ src/sprites.js
@@ -0,0 +1,13 @@
+loadRoot('src/assets/');
+loadSprite('coin', 'coin.png');
+loadSprite('evil-shroom', 'evil-shroom.png');
+loadSprite('brick', 'brick.png');
+loadSprite('block', 'block.png');
+loadSprite('mario', 'mario.png');
+loadSprite('mushroom', 'mushroom.png');
+loadSprite('surprise', 'surprise.png');
+loadSprite('unboxed', 'unboxed.png');
+loadSprite('pipe-top-left', 'pipe-top-left.png');
+loadSprite('pipe-top-right', 'pipe-top-right.png');
+loadSprite('pipe-bottom-left', 'pipe-bottom-left.png');
+loadSprite('pipe-bottom-right', 'pipe-bottom-right.png');
